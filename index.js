c0b8f21093080b04e2a3531b9ae672977c88366f
Clement <gestion@mediasem.com>
2018-08-18 14:55:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const bot = new Discord.Client();
 const cfg = require('./index.json');
 const token = process.env.token;
 const prefix = (cfg.prefix);
-
-
+    
+    
 bot.on('ready', function() {
+    bot.user.setGame("ghashrod-red, /help");
     console.log("Je suis prêt !")
 });
-
+    
 bot.on('message', message => {
     if(message.content === prefix) {
         if(message.content ==='/hello') {
-            message.reply('world !');
+                message.reply('world !');
         }
     }
 });
-
+    
 bot.login(token);
